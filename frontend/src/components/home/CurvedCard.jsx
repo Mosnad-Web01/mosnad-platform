25b877876b1f4eeda90d1ccbade40fd09cb18be7
YasserEsam <yaaser10esam1010@gmail.com>
2024-11-24 00:27:28 +0300

    style: change the color of the curved card

--- frontend/src/components/home/CurvedCard.jsx
+++ frontend/src/components/home/CurvedCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CurvedCard = ({ image, title, subtitle }) => {
   return (
-    <div className="flex gap-6 justify-center items-center bg-white/20 shadow-md rounded-full w-full max-w-xs p-6 hover:shadow-lg transition-shadow duration-300">
+    <div className="flex gap-6 justify-center items-center bg-white/40 shadow-md rounded-full w-full max-w-xs p-6 hover:shadow-lg transition-shadow duration-300">
       <div className="text-center">
         <h3 className="text-lg font-bold text-[#21255C]">{title}</h3>
         <p className="text-base text-[#21255C] font-bold">{subtitle}</p>

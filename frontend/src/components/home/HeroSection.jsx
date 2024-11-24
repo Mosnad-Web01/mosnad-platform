fc2913685849dc536f831fd20e95defcd08cec72
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-24 03:26:30 +0300

    feat :Enhance HeroSection Layout

--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -43,7 +43,7 @@ const HeroSection = () => {
 				<div className="flex flex-col items-center lg:items-start w-full max-w-screen-2xl mx-auto relative">
 					{/* Background Image */}
 					<div
-						className="w-full h-[600px] md:h-[800px] bg-[url('/hero-bg.png')] bg-cover bg-center bg-no-repeat"
+						className="w-full h-[600px] md:h-[700px] bg-[url('/hero-bg.png')] bg-cover bg-center bg-no-repeat"
 						style={{
 							WebkitMaskImage:
 								'linear-gradient(to right, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0) 96%, rgba(0,0,0,0) 100%)',
@@ -56,31 +56,32 @@ const HeroSection = () => {
 						}}></div>
 
 					{/* Content Container */}
-					<div className="absolute right-4 md:right-10 top-1 md:top-4 px-2 md:px-4 pt-10 md:pt-20">
-						<div className="max-w-xl mx-auto text-center flex flex-col gap-2">
+					<div className="absolute right-4 md:right-10 top-4 md:top-2 px-2 md:px-4 pt-10 md:pt-20">
+						<div className="max-w-[650px] mx-auto text-center flex flex-col gap-4">
 							{/* Toggle Buttons */}
-							<div className="w-fit mx-auto my-3 ">
-								<ToggleButtonGroup
-									options={options}
-									activeOption={activeOption}
-									onOptionChange={handleOptionChange}
-									containerStyle="bg-white p-1.5 md:p-2 shadow-sm w-[90%] rounded-xl"
-									buttonStyle="text-base md:text-lg font-bold text-blue-900 transition-all duration-300 hover:shadow-sm flex justify-center gap-2 rounded-full px-3 md:px-4 py-2 md:py-4 w-[120px] sm:w-[150px] md:w-[300px] text-center"
-								/>
-							</div>
+
+              <div className="w-fit mx-auto my-4 shadow-md">
+                <ToggleButtonGroup
+                  options={options}
+                  activeOption={activeOption}
+                  onOptionChange={handleOptionChange}
+                  containerStyle="bg-white p-1.5 md:p-2 shadow-sm w-fit rounded-xl"
+                  buttonStyle="text-base md:text-lg font-bold text-blue-900 transition-all duration-300 hover:shadow-sm flex justify-center gap-2 rounded-full px-3 md:px-4 py-2 md:py-4 w-[120px] sm:w-[150px] md:w-[300px] text-center"
+                />
+              </div>
+
 
 							<h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-blue-900 mb-3 md:mb-6 px-2 leading-normal md:leading-snug lg:leading-relaxed">
 								أهلاً بك في منصة مسند للتدريب والتوظيف
 								<br />
 								<span>نُسند، ونُستند</span>
 							</h1>
-
-							<p className="text-base md:text-lg text-gray-700 mb-6 md:mb-8  leading-relaxed md:leading-loose lg:leading-loose ">
+							
+              <p className="text-base md:text-lg text-gray-700 mb-6 md:mb-8  leading-relaxed md:leading-loose lg:leading-loose ">
 								البحث عن الشخص المناسب بالزمن المناسب، وأنت فيه.
 								حرصنا على احتياجاتك ونلبيه لك بقاعدة بيانات
 								مؤهلة وجاهزة للعمل تحت إشرافنا التام
 							</p>
-
 							{/* CTA Button */}
 							<div className="flex px-4 md:px-0">
 								<button className="w-full py-2 md:py-3 rounded-full bg-gradient text-white text-base md:text-lg font-medium hover:shadow-lg transition-all duration-300 hover:scale-105">

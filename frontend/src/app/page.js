f2ab521ed4fd16cdb22d392ea8ca3d832851b83e
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-26 06:59:43 +0300

    feat: implement responsive CareerPathSection
    - Responsive layout with optimized `CareerPathCard`.
    - Localized Arabic text for better user relevance.
    - Interactive buttons and links to boost engagement.
    - Optimized image handling for performance.
    - Enhanced accessibility and visual appeal.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,16 +1,16 @@
-
-import Pricing from "../app/prices/page";
-import AboutSection from "@/components/home/AboutSection";
-import HeroSection from "@/components/home/HeroSection";
-import ServedCompaniesSection from "@/components/home/ServedCompaniesSection";
+import Pricing from '../app/prices/page';
+import AboutSection from '@/components/home/AboutSection';
+import CareerPathSection from '@/components/home/CareerPathSection';
+import HeroSection from '@/components/home/HeroSection';
+import ServedCompaniesSection from '@/components/home/ServedCompaniesSection';
 
 export default function Home() {
-  return (
-    <>
-      <HeroSection />
-      <AboutSection />
-      <Pricing />
-      <ServedCompaniesSection />
-    </>
-  );
+	return (
+		<>
+			<HeroSection />
+			<AboutSection />
+			<ServedCompaniesSection />
+			<CareerPathSection />
+		</>
+	);
 }

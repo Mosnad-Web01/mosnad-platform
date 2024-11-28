f5c5e168440b553af179ef0346d021d4179430b7
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-28 18:07:49 +0300

    feat(users): extend users table with missing fields - Added `phone_number`, `role_id`, `status`, and `deleted_at` columns to the `users` table - Implemented soft deletes with `deleted_at` - Updated `User` model to support new columns - Updated `UserFactory` model to support new columns - Seeding User table with 1 admin 1 company and 1 student

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,22 +6,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+   protected $fillable = [
+        'name', 'email', 'password', 'phone_number', 'role_id', 'status'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -43,6 +41,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'status' => 'string', 
         ];
     }
 }

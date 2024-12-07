811c5df673dc57fc097e3e1cfd209240e36189d7
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-12-07 20:58:55 +0300

    feat: enhance JobOpportunityController to handle image URLs
    - Updated index method to process image URLs for job opportunities.
    - Added logic to check if the image URL is external or local.
    - Generated full URLs for local storage images.
    - Set imgurl to null if no image URL exists.

--- backend/app/Http/Controllers/api/JobOpportunityController.php
+++ backend/app/Http/Controllers/api/JobOpportunityController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\ContactUs;
-use Illuminate\Http\Request;
 use App\Models\JobOpportunity;
 
-class JobOpportunityController  extends Controller
-{
 
+class JobOpportunityController extends Controller
+{
     public function index()
     {
-        $opportunities = JobOpportunity::all();
+        //pass full url by modifying imageUrl in cases of local storage and external url
+        $opportunities = JobOpportunity::all()->map(function ($opportunity) {
+            if ($opportunity->imgurl) {
+                // check if the URL is an external URL
+                if (filter_var($opportunity->imgurl, FILTER_VALIDATE_URL)) {
+                    $opportunity->imgurl ?? $opportunity->imgurl;
+                } else {
+                    // generate the full URL for local storage
+                    $opportunity->imgurl = "http://127.0.0.1:8000/storage/" . $opportunity->imgurl;
+                }
+            } else {
+                $opportunity->imgurl = null;
+            }
+
+            return $opportunity;
+        });
+
         return response()->json([
             'opportunities' => $opportunities,
         ], 200);
